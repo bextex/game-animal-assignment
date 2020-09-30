b38cd5cac80b59a9c98334296098b08460114e8c
bextex <r.sarvegard@gmail.com>
2020-09-30 09:38:55 +0200

    Testing all the newly created animals to eat grass

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,21 @@ public class Main {
     public static void main(String[] args) {
 
         Cow carola = new Cow("carOLA", "male");
+        Fish nemo = new Fish("nemo", "male");
+        Horse leyla = new Horse("Leyla", "female");
+        Rabbit snurre = new Rabbit("Snurre", "male");
+        Ostrich ida = new Ostrich("ida", "female");
+        System.out.println();
         Grass grass = new Grass();
         carola.eatsFood(grass);
+        System.out.println();
+        nemo.eatsFood(grass);
+        System.out.println();
+        leyla.eatsFood(grass);
+        System.out.println();
+        snurre.eatsFood(grass);
+        System.out.println();
+        ida.eatsFood(grass);
 
 
     }
