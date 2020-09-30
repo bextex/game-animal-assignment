738c6272b4a688bdd11195337f00f8232f1826eb
bextex <r.sarvegard@gmail.com>
2020-09-30 12:51:22 +0200

    The players created adds in ArrayList and get welcomed for the first round in Games main method play(). Store class added

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -7,26 +7,40 @@ import java.util.Scanner;
 public class Game {
     Random random = new Random();
     Scanner input = new Scanner(System.in);
+    int round = random.nextInt(25) + 5;
+    boolean firstRound = true;
 
 
     public Game(){
         System.out.println("Hello and welcome to the game PiggyBank!\n");
         sleep(1000);
         System.out.println("In this game you can play by yourself or with up to three friends.");
-        System.out.println("To make it extra fun, we - the game masters - will choose between 5 and 30 rounds so the game stops when you least expect it... Good luck!\n");
+        System.out.println("To make it extra fun, we - the game masters - will choose between 5 and 30 rounds so the game stops when you least expect it... Good luck!");
         System.out.println("Press ENTER when you're ready to start.");
         input.nextLine();
         System.out.println("Choose how many players you want to be (1-4)");
-        String players = input.nextLine(); // Make som sort of control so there cannot be more than 4 or less than 1 player
-        setPlayers(players);
+        String playerNum = input.nextLine(); // -----Make som sort of control so there cannot be more than 4 or less than 1 player-----
+        setPlayers(playerNum);
+        sleep(1000);
+        play();
     }
 
-    public void setPlayers(String players){
-        int numOfPlayers = Integer.parseInt(players);
+    public void setPlayers(String playerNum){
+        int numOfPlayers = Integer.parseInt(playerNum);
         for(int i = 1; i <= numOfPlayers; i++){
             System.out.println("Type in the name of Player " + i + ":");
             String player = input.nextLine();
-            new Player(player);
+            Store.players.add(new Player(player));
+        }
+    }
+
+    public void play(){ // The main playing field where all choices will be made and continue until the game is over
+        if(firstRound){
+            System.out.print("Welcome ");
+            for(Player p : Store.players){
+                System.out.print(p.name); // -----Fix formatting when welcoming the players!!!-----
+            }
+            firstRound = false;
         }
     }
 
--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -1,4 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Store {
+
+    public static ArrayList<Player> players = new ArrayList<>();
+    public static ArrayList<Animal> animals;
+    public static ArrayList<Food> foods;
+
+    public int animalCost;
+    public String animalName;
+
+    public void buyAnimal(Player player, Animal animal){
+        if(player.money > animalCost) {
+            //animals.add(player);
+            //players.add(animal);
+            System.out.println("You have now successfully bought");
+        }
+    }
+
+    public void buyFood(Player player, Food food){
+
+    }
+
 }
