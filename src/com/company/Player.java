c58fbf24a3556f6cff4e0a022e3849bba88e0eed
bextex <r.sarvegard@gmail.com>
2020-10-01 15:44:38 +0200

    Moved buyFood and buyAnimal to Store. Added LinkedHashmap in Player to store food type and kg of each food

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,17 +1,32 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class Player {
 
     public static ArrayList<Player> players = new ArrayList<>();
     ArrayList<Animal> animals = new ArrayList<>();
-    ArrayList<Food> foods =  new ArrayList<>();
+    LinkedHashMap<String, Integer> foods = new LinkedHashMap<>();
 
     public String name;
     public int money = 1000;
 
     public Player(String name){
         this.name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
+
+    public void addFood(String choice, int foodInKg) {
+        if(!foods.containsKey(choice)){
+            foods.put(choice, foodInKg);
+        } else {
+            int value = foods.get(choice);
+            int newValue = value + foodInKg;
+            foods.put(choice, newValue);
+        }
+        for(String key : foods.keySet()){
+            System.out.println(key + ": " + (Integer)foods.get(key));
+        }
+
     }
 }
--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -1,24 +1,84 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Store {
 
-
+    Scanner input = new Scanner(System.in);
+    public static ArrayList<Player> players = new ArrayList<>();
+    public static ArrayList<Animal> animals = new ArrayList<>();
+    public static ArrayList<Food> foods = new ArrayList<>();
 
     public int animalCost;
     public String animalName;
-/*
-    public static void buyAnimal(Player player, Animal animal){
-        if(player.money > animalCost) {
-            //animals.add(player);
-            //players.add(animal);
-            System.out.println("You have now successfully bought");
+
+    public void buyAnimal(Player player) throws Exception{
+        System.out.println("What animal would you like to buy? Cow[1], fish[2], horse[3], rabbit[4] or ostrich[5]?");
+        String choice = input.nextLine().toLowerCase();
+        System.out.println("Type the name of your animal.");
+        String animalName = input.nextLine();
+        System.out.println("Type in female[1] or male[2] for gender of your animal.");
+        String animalGender = input.nextLine();
+        animalGender = animalGender.equals("1") ? "female" : "male";
+        if(choice.contains("cow") || choice.contains("1")){
+            player.animals.add(new Cow(animalName, animalGender));
+        } else if(choice.contains("fish") || choice.contains("2")){
+            player.animals.add(new Fish(animalName, animalGender));
+        } else if(choice.contains("horse") || choice.contains("3")){
+            player.animals.add(new Horse(animalName, animalGender));
+        } else if(choice.contains("rabbit") || choice.contains("4")){
+            player.animals.add(new Rabbit(animalName, animalGender));
+        } else if(choice.contains("ostrich") || choice.contains("5")){
+            player.animals.add(new Ostrich(animalName, animalGender));
+        } else{
+            throw new Exception("Not valid.");
         }
     }
 
-    public void buyFood(Player player, Food food){
+    public void buyFood(Player player) throws Exception {
+        while(true) {
+            System.out.println("What food would you like to buy? Grass[1], corn[2], meat[3] or exit[4]?");
+            String choice = input.nextLine().toLowerCase(); // Add try-catch
+            if (choice.contains("exit") || choice.contains("4")) {
+                break;
+            }
+            int idPricePerKg;
+            System.out.println("How much " + choice + " would you like to buy in kg?");
+            int foodInKg = Integer.parseInt(input.nextLine()); // Add try-catch
+            if(player.money < (foodInKg * Grass.pricePerKg) || player.money < (foodInKg * Corn.pricePerKg)
+                    || player.money < (foodInKg * Meat.pricePerKg)){
+                System.out.println("You don't have enough money.");
+                break;
+            }
+            if (choice.contains("grass") || choice.contains("1")) {
+                choice = "grass";
+                idPricePerKg = Grass.pricePerKg;
+                // add "grass" as food if not added, change kg of food the player have
+            } else if (choice.contains("corn") || choice.contains("2")) {
+                choice = "corn";
+                idPricePerKg = Corn.pricePerKg;
+            } else if (choice.contains("meat") || choice.contains("3")) {
+                choice = "meat";
+                idPricePerKg = Meat.pricePerKg;
+            } else {
+                throw new Exception("Not valid.");
+            }
+            player.money = player.money - Math.round(foodInKg * idPricePerKg);
+            player.addFood(choice, foodInKg);
+            System.out.println("You bought " + foodInKg + " kg of " + choice + ". " + "You now have " + player.money + " kr left.");
+            sleep(500);
+        }
 
     }
-*/
+
+    private void sleep(int ms){
+        try {
+            Thread.sleep(ms);
+        }
+        catch(Exception ignore){}
+    }
+
+
+
 }
