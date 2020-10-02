72ddec10b48e34896a55b7a817fdc15db7417690
bextex <r.sarvegard@gmail.com>
2020-10-02 14:03:06 +0200

    Created sellAnimal in Store

--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -41,6 +41,21 @@ public class Store {
         }
     }
 
+    public void sellAnimal(Player player){
+        System.out.println("Type in the name of your animal you want to sell.");
+        this.animalName = input.nextLine();
+        for (int i = 0; i < player.animals.size(); i++) {
+            if (animalName.equals(player.animals.get(i).name)) {
+                this.animal = player.animals.get(i);
+                player.animals.remove(animal);
+                player.money = player.money + (animal.price * animal.health);
+            } else {
+                System.out.println("You can't sell an animal you don't own.");
+            }
+        }
+        System.out.println("Congratulations on your sell. You now have " + player.money + "!");
+    }
+
     public void buyFood(Player player) throws Exception {
         System.out.println("What food would you like to buy? Grass[1], corn[2], meat[3] or exit[4] to get back to menu.");
         String choice = input.nextLine().toLowerCase();
@@ -67,88 +82,7 @@ public class Store {
         }
 
     }
-        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        System.out.println("What animal would you like to buy? Cow[1], fish[2], horse[3], rabbit[4] or ostrich[5]?");
-        String choice = input.nextLine().toLowerCase();
-        System.out.println("Type the name of your animal.");
-        String animalName = input.nextLine();
-        System.out.println("Type in female[1] or male[2] for gender of your animal.");
-        String animalGender = input.nextLine();
-        animalGender = animalGender.equals("1") ? "female" : "male";
-        if(choice.contains("cow") || choice.contains("1")){
-            player.animals.add(new Cow(animalName, animalGender));
-        } else if(choice.contains("fish") || choice.contains("2")){
-            player.animals.add(new Fish(animalName, animalGender));
-        } else if(choice.contains("horse") || choice.contains("3")){
-            player.animals.add(new Horse(animalName, animalGender));
-        } else if(choice.contains("rabbit") || choice.contains("4")){
-            player.animals.add(new Rabbit(animalName, animalGender));
-        } else if(choice.contains("ostrich") || choice.contains("5")){
-            player.animals.add(new Ostrich(animalName, animalGender));
-        } else{
-            throw new Exception("Not valid.");
-        }
-    } */
-
-    /*
-    public void buyFood(Player player) throws Exception {
-        while(true) {
-            System.out.println("What food would you like to buy? Grass[1], corn[2], meat[3] or exit[4]?");
-            String choice = input.nextLine().toLowerCase(); // Add try-catch
-            if (choice.contains("exit") || choice.contains("4")) {
-                break;
-            }
-            int idPricePerKg;
-            System.out.println("How much " + choice + " would you like to buy in kg?");
-            int foodInKg = Integer.parseInt(input.nextLine()); // Add try-catch
-            if(player.money < (foodInKg * Grass.pricePerKg) || player.money < (foodInKg * Corn.pricePerKg)
-                    || player.money < (foodInKg * Meat.pricePerKg)){
-                System.out.println("You don't have enough money.");
-                break;
-            }
-            if (choice.contains("grass") || choice.contains("1")) {
-                choice = "grass";
-                idPricePerKg = Grass.pricePerKg;
-                // add "grass" as food if not added, change kg of food the player have
-            } else if (choice.contains("corn") || choice.contains("2")) {
-                choice = "corn";
-                idPricePerKg = Corn.pricePerKg;
-            } else if (choice.contains("meat") || choice.contains("3")) {
-                choice = "meat";
-                idPricePerKg = Meat.pricePerKg;
-            } else {
-                throw new Exception("Not valid.");
-            }
-            player.money = player.money - Math.round(foodInKg * idPricePerKg);
-            //player.addFood(food, foodInKg);
-            System.out.println("You bought " + foodInKg + " kg of " + choice + ". " + "You now have " + player.money + " kr left.");
-            sleep(500);
-        }
-
-    }
-*/
     private void sleep(int ms){
         try {
             Thread.sleep(ms);
